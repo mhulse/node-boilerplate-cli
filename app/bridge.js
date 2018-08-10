ae9df4bdd3d5e3da733b28c25c342d73432c52f3
Micky Hulse <mickyhulse@gmail.com>
2018-08-09 23:36:59 -0700

    SAVEPOINT

--- app/bridge.js
+++ app/bridge.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 // jshint esversion:6
 
-require('babel-register');
+// https://stackoverflow.com/a/35120765/922323
+require('babel-register')({
+  ignore: false,
+  only: './app'
+});
 require('./index');
--- app/utilities.js
+++ app/utilities.js
@@ -5,59 +5,59 @@ import fs from 'fs-extra';
 import colors from 'colors';
 
 module.exports = {
-	
-	fileExists: function(filePath) {
-		
-		try {
-			return fs.statSync(filePath).isFile();
-		} catch (error) {
-			return false;
-		}
-		
-	},
-	
-	dirExists: function(dirPath) {
-		
-		try {
-			return fs.statSync(dirPath).isDirectory();
-		} catch (error) {
-			return false;
-		}
-		
-	},
-	
-	title: function(string) {
-		
-		this.o('log');
-		this.o('log', ` ${string.toUpperCase()} `.bold.bgBlack.white);
-		this.o('log');
-		
-	},
-	
-	exitGraceful: function(exitCode = 0) {
-		
-		process.exitCode = exitCode;
-		
-	},
-	
-	done: function({
-		text = 'Done!',
-		before = true,
-		after = false,
-	} = {}) {
-		
-		if (before) this.o('log');
-		
-		this.o('log', text);
-		
-		if (after) this.o('log');
-		
-	},
-	
-	o: function(type, ... messages) {
-		
-		console[type].apply(this, messages);
-		
-	},
-	
+
+  fileExists: function(filePath) {
+
+    try {
+      return fs.statSync(filePath).isFile();
+    } catch (error) {
+      return false;
+    }
+
+  },
+
+  dirExists: function(dirPath) {
+
+    try {
+      return fs.statSync(dirPath).isDirectory();
+    } catch (error) {
+      return false;
+    }
+
+  },
+
+  title: function(string) {
+
+    this.o('log');
+    this.o('log', ` ${string.toUpperCase()} `.bold.bgBlack.white);
+    this.o('log');
+
+  },
+
+  exitGraceful: function(exitCode = 0) {
+
+    process.exitCode = exitCode;
+
+  },
+
+  done: function({
+    text = 'Done!',
+    before = true,
+    after = false,
+  } = {}) {
+
+    if (before) this.o('log');
+
+    this.o('log', text);
+
+    if (after) this.o('log');
+
+  },
+
+  o: function(type, ... messages) {
+
+    console[type].apply(this, messages);
+
+  },
+
 };
